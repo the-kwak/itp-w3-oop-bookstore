66e59b832f0b0fddf86d62ec47daaef65b811032
Khristopher Chireno <kcmastermind2005@gmail.com>
2017-09-22 10:30:44 +0000

    completed bookstore

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -2,6 +2,7 @@ class Bookstore(object):
     def __init__(self, bookstore_name):
         self.name = bookstore_name
         self.book_inventory = []
+        self.results =[]
         
     def add_book(self, book):
         # Take in book variable and append to our book inventory
@@ -10,7 +11,22 @@ class Bookstore(object):
     
     def get_books(self):
         return self.book_inventory
-
+    
+    def search_books(self, author = None, title = None):
+        self.results = []
+        if title != None:
+            for book in self.book_inventory:
+                if book.title.startswith(title.title(), 0 , len(title)) :
+                    self.results.append(book)
+                    
+        elif author != None:
+            self.results
+            for book in self.book_inventory:
+                if author is book.author:
+                    self.results.append(book)
+        return self.results
+    
+    
 class Author(object):
     def __init__ (self, name, nationality):
         self.name = name
