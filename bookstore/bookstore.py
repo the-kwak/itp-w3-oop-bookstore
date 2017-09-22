29b9e96fb8d75b6e0b642f16dd01b9423935006b
Khristopher Chireno <kcmastermind2005@gmail.com>
2017-09-22 03:03:29 +0000

    Author and Book classes test passed

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,33 @@
 class Bookstore(object):
-    pass
-
+    def __init__(self, bookstore_name):
+        self.name = bookstore_name
+        self.book_inventory = []
+        
+    def add_book(self, book):
+        # Take in book variable and append to our book inventory
+        self.book_inventory.append(book)
+        
+    
+    def get_books(self):
+        return self.book_inventory
 
 class Author(object):
-    pass
-
+    def __init__ (self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+        # initialize a list of books for the author
+        self.list_of_books = []
+        
+    def get_books(self):
+        # Return the list of books for the author
+        return self.list_of_books
+        
+        
 
 class Book(object):
-    pass
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        # Takes the author object and adds the current book to the list in the
+        # Author object
+        author.list_of_books.append(self)
